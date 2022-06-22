1f406cd1fd33d0ec727cf9c25f6719b83636d4ef
pankaj779 <84573170+pankaj779@users.noreply.github.com>
2022-06-22 10:23:21 +0530

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://cors-anywhere.herokuapp.com/https://pankaj.rtd.squareops.co.in/api';
+const API_ROOT = 'https://pankaj.rtd.squareops.co.in/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;

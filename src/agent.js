ec292f790a8dce17cc52468a46d7ae7f424b2cee
pankaj779 <84573170+pankaj779@users.noreply.github.com>
2022-07-23 08:13:58 +0530

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://pankaj.rtd.squareops.co.in/api';
+const API_ROOT = 'http://pankaj.bookmyzym.com/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
